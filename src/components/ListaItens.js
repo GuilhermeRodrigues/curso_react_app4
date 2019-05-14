fe3a3fa1123f926e656274e8630287400483c3f3
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-05-14 13:27:45 -0300

    Mounting Cycle
    Ciclo de vida de um componente.

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -5,7 +5,22 @@ import {
 } from 'react-native';
 
 export default class ListaItens extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('Construindo a aplicação');
+    }
+
+    componentWillMount() {
+        console.log('Fazer alguma coisa antes de renderizar');
+    }
+
+    componentDidMount() {
+        console.log('Fazer alguma coisa depois de renderizar');
+    }
+
     render() {
+        console.log('Objeto é renderizado');
         return (
             <Text>Teste de importação de componente</Text>
         );
