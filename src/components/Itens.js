8bdad02c5a70736856b365461c817e930d4e0abb
GuilhermeRodrigues <guilherme17_rodrigues@hotmail.com>
2019-05-14 22:13:05 -0300

    Criando o componente Itens
    Criando o componente Itens.

---
+++ src/components/Itens.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import {
+    Text
+} from 'react-native';
+
+export default class Itens extends Component {
+
+    render() {
+        console.log('Objeto é renderizado');
+        return (
+            <Text>Detalhes do item...</Text>
+        );
+    }
+}
--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
 
 import {
-    Text
+    View
 } from 'react-native';
 
+import Itens from './Itens';
+
 export default class ListaItens extends Component {
-
-    constructor(props) {
-        super(props);
-        console.log('Construindo a aplicação');
-    }
-
-    componentWillMount() {
-        console.log('Fazer alguma coisa antes de renderizar');
-    }
-
-    componentDidMount() {
-        console.log('Fazer alguma coisa depois de renderizar');
-    }
 
     render() {
         console.log('Objeto é renderizado');
         return (
-            <Text>Teste de importação de componente</Text>
+            <View>
+                <Itens />
+                <Itens />
+                <Itens />
+            </View>
         );
     }
 }
